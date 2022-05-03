611ab76cb946488ef4c7a6ac711eac8be0d5a269
Rodrigo de la Riva <100295171+Rodrigo-de-la-Riva@users.noreply.github.com>
2022-05-03 09:45:53 -0600

    comit7
    se modifico el archivo main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 print('Nombre: Rodrigo Alejandro de la Riva de la Roca')
 print('Carnet: 1527122')
 print('Carrera: Ingernieria en Informatica y Sistemas')
+
+p = input('ingrese su animal favorito: ')
+if p == 'perro':
+    print('A mi tambien me gustan los perros! :D')
+else:
+    print('Interesante eleccion, no me agrada, ni me desagrada, pero prefiero los perros')
